38b5cc7c35b27af017fceeb3d2f71b1d1f08d339
Imdad Ahmed <imdhmd@gmail.com>
2014-10-29 19:30:18 +0530

    an endpoint to test an async http call to external service

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### Proof of concept - Asynchronous request handling using spring mvc
 
-##### Testing:
+##### Testing by invoking a new thread:
 1. Run the app: `mvn jetty:run` and notice the logs (on the console itself)
 1. Open a new terminal and split it to four panes (like in iterm cmd+t and then cmd+d 3 times)
 1. Execute the following 4 curl requests from those 4 respective terminal panes at the same time (i do this in iterm using alt+cmd+i -> input goes to all open terminals)
@@ -18,3 +18,11 @@ Support for asynchronous request handling allows you to defer the response compu
 References:
 * http://docs.spring.io/spring/docs/current/spring-framework-reference/html/mvc.html#mvc-ann-async
 * http://stackoverflow.com/questions/13646428/issue-with-making-a-controller-method-asynchronous-with-spring-3-2-rc2
+
+##### Update (29 Oct, 2014)
+
+* Added an async http call (via spring's http client) to an external service (wiki API)
+* If i set the number worker threads (that handle request) to 1, i can see that all the requests are handled immediately but:
+  * The callback from async http request is being handled by a new thread every time
+  * Who is spawning this new thread?
+  * Is not this async http call supposed to be via evented io?
--- src/main/java/com/imdhmd/spring/async/controllers/AsyncReqHandler.java
+++ src/main/java/com/imdhmd/spring/async/controllers/AsyncReqHandler.java
@@ -1,11 +1,19 @@
 package com.imdhmd.spring.async.controllers;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.AsyncClientHttpRequest;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsAsyncClientHttpRequestFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.util.concurrent.FailureCallback;
+import org.springframework.util.concurrent.SuccessCallback;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.async.DeferredResult;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Scanner;
 
 import static java.lang.String.format;
 
@@ -14,12 +22,13 @@ public class AsyncReqHandler {
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     @ResponseBody
-    public DeferredResult<String> handle(@PathVariable("id") final String id){
-        System.out.println(format("Handling request for %s", id));
-        final DeferredResult<String> deferredResult = new DeferredResult<String>();
+    public DeferredResult<String> handle(@PathVariable("id") final String id) {
+        System.out.println(format("Handling request for %s, thread id: %s", id, Thread.currentThread().getId()));
+        final DeferredResult<String> deferredResult = new DeferredResult<>();
 
         defer(id, deferredResult);
 
+        System.out.println(format("I'm returning from %s, thread id: %s", id, Thread.currentThread().getId()));
         return deferredResult;
     }
 
@@ -29,12 +38,46 @@ public class AsyncReqHandler {
             public void run() {
                 try {
                     Thread.sleep(2500);
-                    System.out.println(format("Setting result for %s", id));
+                    System.out.println(format("Setting result for %s, thread id: %s", id, Thread.currentThread().getId()));
                     deferredResult.setResult(format("Result for %s\r\n", id));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         }).start();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/wiki")
+    @ResponseBody
+    public DeferredResult<String> wiki(@RequestParam("q") final String q) throws URISyntaxException, IOException {
+        System.out.println(format("Wiki query for %s, thread id: %s", q, Thread.currentThread().getId()));
+        final DeferredResult<String> deferredResult = new DeferredResult<>();
+
+        AsyncClientHttpRequest asyncRequest = new HttpComponentsAsyncClientHttpRequestFactory()
+                .createAsyncRequest(new URI(format("http://en.wikipedia.org/w/api.php?format=json&action=query&titles=%s", q)), HttpMethod.GET);
+        asyncRequest.executeAsync().addCallback(new SuccessCallback<ClientHttpResponse>() {
+            @Override
+            public void onSuccess(ClientHttpResponse clientHttpResponse) {
+                try {
+                    Thread thread = Thread.currentThread();
+                    System.out.println(
+                            format("Setting result for wiki query %s, thread id: %s, thread name: %s, thread: %s, isDaemon: %s",
+                                    q, thread.getId(), thread.getName(), thread.toString(), thread.isDaemon()));
+
+                    Scanner scanner = new Scanner(clientHttpResponse.getBody());
+                    deferredResult.setResult(scanner.useDelimiter("\\A").hasNext() ? scanner.next() : "");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new FailureCallback() {
+            @Override
+            public void onFailure(Throwable throwable) {
+                deferredResult.setErrorResult(throwable);
+            }
+        });
+
+        System.out.println(format("I'm returning from %s, thread id: %s", q, Thread.currentThread().getId()));
+        return deferredResult;
     }
 }
